e33bf002caec695a8cf054b07cfa50dc0769e2e3
Mikko Koski <mikkokos@gmail.com>
2014-10-06 20:44:48 +0300

    Added Solarized

--- init.el
+++ init.el
@@ -1,1 +1,22 @@
 ;; init.el
+
+;; Package managers
+
+(require 'package)
+(setq package-archives
+      '(("marmalade" . "http://marmalade-repo.org/packages/")
+        ;; ("tromey" . "http://tromey.com/elpa/")
+    ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
+;; My installed packages
+
+(defvar my-packages '(color-theme-solarized))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+;; Theme
+
+(load-theme 'solarized-dark t)
