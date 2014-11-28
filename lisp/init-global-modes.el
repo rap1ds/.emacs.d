0bf675f87e69e8e3b352da9ee0cde0884c6770d3
Mikko Koski <mikkokos@gmail.com>
2014-11-29 00:08:04 +0200

    Smart parens

--- lisp/init-global-modes.el
+++ lisp/init-global-modes.el
@@ -1,8 +1,11 @@
 (require-package 'rainbow-delimiters)
+(require-package 'smartparens)
 
 (global-linum-mode 1)
 
 ;; Enable in all programming related modes
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(smartparens-global-mode 1)
 
 (provide 'init-global-modes)
