9005dc60c5618a632ca9b72a127bbdc00da8aa54
Mikko Koski <mikkokos@gmail.com>
2014-11-28 23:47:07 +0200

    Rainbox delimiters

--- lisp/init-global-modes.el
+++ lisp/init-global-modes.el
@@ -1,3 +1,8 @@
+(require-package 'rainbow-delimiters)
+
 (global-linum-mode 1)
+
+;; Enable in all programming related modes
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 (provide 'init-global-modes)
