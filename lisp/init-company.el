4b8606d74a9bdc4cd4e7d7382272e5d239b5b295
Mikko Koski <mikkokos@gmail.com>
2017-12-31 22:42:40 +0200

    Navigate company mode suggestions with C-n C-p

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -4,4 +4,9 @@
 (setq company-idle-delay 0.3)
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Navigate suggestions up and down with C-n and C-p
+;; Source: https://github.com/purcell/emacs.d/blob/37532516475d13531dd287d6b11ea4bc03ecfcbc/lisp/init-company.el#L13
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
 (provide 'init-company)
