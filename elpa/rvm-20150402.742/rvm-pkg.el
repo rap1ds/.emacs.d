14e31c4104dfc88387670263726fa6c3d3152ed6
Mikko Koski <mikkokos@gmail.com>
2018-07-24 11:58:43 +0300

    Include installed packages to repository
    This may come handy if an upgrade breaks things.

---
+++ elpa/rvm-20150402.742/rvm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rvm" "20150402.742" "Emacs integration for rvm" 'nil :commit "134497bc460990c71ab8fa75431156e62c17da2d" :url "http://www.emacswiki.org/emacs/RvmEl" :keywords '("ruby" "rvm"))
